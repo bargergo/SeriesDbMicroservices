58993be7e45a2a3b7a21f79470fa987f5e2cf5a3
bargergo <bargergo@gmail.com>
2020-04-10 23:58:12 +0200

    Build gradle in docker

--- RatingsMicroservice/src/Application.kt
+++ RatingsMicroservice/src/Application.kt
@@ -18,7 +18,6 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
-    install(DefaultHeaders)
     install(CallLogging) {
         level = Level.INFO
         filter { call -> call.request.path().startsWith("/") }
