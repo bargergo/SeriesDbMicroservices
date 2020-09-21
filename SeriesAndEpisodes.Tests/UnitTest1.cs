a49aa81870535f1636a3e1563f61aeb54e731565
bargergo <bargergo@gmail.com>
2020-09-21 17:17:41 +0200

    Added dummy tests for SeriesAndEpisodes

---
+++ SeriesAndEpisodes.Tests/UnitTest1.cs
@@ -0,0 +1,23 @@
+using System;
+using FluentAssertions;
+using Xunit;
+
+namespace SeriesAndEpisodes.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            var number = 1;
+            number.Should().Be(1);
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            var number = 1;
+            number.Should().Be(2);
+        }
+    }
+}
