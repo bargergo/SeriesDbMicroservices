a57947ba19ef45f8dfe136bce79fd80465b85e63
bargergo <bargergo@gmail.com>
2020-10-12 17:12:59 +0200

    Move tests to a folder

--- webclient/src/__tests__/App.test.tsx
+++ webclient/src/__tests__/App.test.tsx
@@ -1,6 +1,6 @@
 import { render } from "@testing-library/react";
 import React from "react";
-import Home from "./components/Home";
+import Home from "../components/Home";
 
 it("should render", () => {
   const { getByText } = render(<Home />);
