85d5e0aab6ed33554e5029fd0d44b079dcde5f7b
bargergo <bargergo@gmail.com>
2020-12-21 12:45:15 +0100

    Wait for user db

--- UserServer/Program.cs
+++ UserServer/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
 using UserServer.Database;
 
 namespace UserServer
@@ -13,7 +15,21 @@ namespace UserServer
             var webHost = CreateHostBuilder(args).Build();
             using (var scope = webHost.Services.CreateScope())
             {
-                scope.ServiceProvider.GetRequiredService<UserDbContext>().Database.Migrate();
+                var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+                while (true)
+                {
+                    try
+                    {
+                        scope.ServiceProvider.GetRequiredService<UserDbContext>().Database.Migrate();
+                        break;
+                    }
+                    catch (Exception e)
+                    {
+                        logger.LogInformation($"Waiting for db... cause:{e.Message}");
+                        System.Threading.Thread.Sleep(1000);
+                    }
+                }
+
             }
             webHost.Run();
         }
